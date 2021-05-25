933bc6bcd0d8079ab6029babc818e74496c5de9b
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-25 21:34:09 +0200

    add comment to divide sent.in code

--- turtle.py
+++ turtle.py
@@ -2,5 +2,13 @@ import turtle
 import random
 import sys
 
-animal = turtle.Turtle()
-turtle.shape("turtle")
+
+# initialization elements..
+t = turtle.Turtle()
+t.shape("turtle")
+
+# loop with main sentences..
+
+
+# exit from window with turtle module..
+turtle.exitonclick()
