a728cb273ce8c93ba3f8b24634453adaab84e2eb
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-27 21:01:36 +0200

    update menu with rename variables

--- origin.py
+++ origin.py
@@ -2,7 +2,8 @@ import turtle_mode
 
 print("..Main panel control..")
 
+print("...MENU...OPTIONS...")
 print(" -[1]- Flower circle loop ")
 print(" -[2]- Secret turtle loop ")
 
-way = int(input("Your choice form meny ==> "))
+way_user = int(input("Your choice form meny ==> "))
