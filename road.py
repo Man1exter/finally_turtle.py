64dd11e134d3e5d473144d3e0f2a28b063cffa63
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-30 11:41:10 +0200

    setting is already done

--- road.py
+++ road.py
@@ -1,3 +1,20 @@
 import random
 import time
 import turtle
+
+# settings
+user = turtle.Turtle()
+user.shape("turtle")
+user.color("green")
+user.pensize(5)
+turtle.bgcolor("black")
+turtle.title("who's first at the finish line?")
+
+# players
+
+# start
+
+# finish
+
+# close / exit
+turtle.exitonclick()
