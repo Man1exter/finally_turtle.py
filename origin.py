01ebb286307a3775f15f045de306a3c0144e5a89
Mariusz Perzynski <m.perzyk@interia.pl>
2021-06-05 15:35:04 +0200

    end of control panel with suitable option

--- origin.py
+++ origin.py
@@ -6,6 +6,13 @@ print("..Main panel control..")
 print("...MENU...OPTIONS...")
 print(" -[1]- Flower Circle Loop ")
 print(" -[2]- Secret Turtle Loop ")
-print(" -[2]- Small Turtle Loops ")
+print(" -[3]- Small Turtle Loops ")
 
 way_user = int(input("Your choice from menu ==> "))
+
+if way_user == 1:
+    turtle_mode
+elif way_user == 2:
+    road
+elif way_user == 3:
+    print("..SOON..")
